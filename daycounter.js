56bbd3db7f1a96256012c7b7c3c4bc31c908f39a
xh244 <xh244@cornell.edu>
2024-05-18 19:55:08 -0700

    Convert months to days; update color

--- daycounter.js
+++ daycounter.js
@@ -1,3 +1,35 @@
+const monthToDays = {
+    1: 31,
+    2: 28,
+    3: 31,
+    4: 30,
+    5: 31,
+    6: 30,
+    7: 31, 
+    8: 31,
+    9: 30,
+    10: 31,
+    11: 30,
+    12: 31
+}
+
+function isLeapYear(year) {
+    if (year % 4 === 0) {
+        if (year % 100 === 0) {
+            if (year % 400 === 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return true;
+        }
+    } else {
+        return false;
+    }
+}
+
+
 function calculateTimeSince(startDate) {
     const start = new Date(startDate);
     const today = new Date();
@@ -27,18 +59,30 @@ function calculateTimeSince(startDate) {
         days += previousMonth.getDate();
         months--;
     }
+
     if (months < 0) {
         months += 12;
         years--;
     }
+    for (let i = 0; i < months; i++) {
+        let currMonth = 6 + i; 
+        if (currMonth > 12) {
+            currMonth -= 12;
+        }
+        days += monthToDays[currMonth];
+        if (isLeapYear(today.getFullYear()) && currMonth == 2) {
+            days += 1;
+            console.log("leap year")
+        }
+    }
 
-    return { years, months, days, hours, minutes, seconds };
+    return { years, days, hours, minutes, seconds };
 }
 
 function updateTime() {
-    const { years, months, days, hours, minutes, seconds } = calculateTimeSince('2015-05-26');
+    const { years, days, hours, minutes, seconds } = calculateTimeSince('2015-05-26');
     document.getElementById('year-number').innerText = years
-    document.getElementById('month-number').innerText = months
+    // document.getElementById('month-number').innerText = months
     document.getElementById('day-number').innerText = days
     document.getElementById('hour-number').innerText = hours
     document.getElementById('minute-number').innerText = minutes
